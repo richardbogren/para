59b05f3e90d2bbaa53c8fbdcb13e1b4fc148f0ed
richardbogren <56296742+richardbogren@users.noreply.github.com>
2024-12-11 14:00:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -195,6 +195,7 @@ if (mobileCheck == false) {
                                     invertX: true,
                                     invertY: false
                                 });
+                                console.log(parallaxInstance);
                             });
                         }
                     })
